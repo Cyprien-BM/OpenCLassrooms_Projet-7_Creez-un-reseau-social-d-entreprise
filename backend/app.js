062b5beab4a95cd28b0bab2ff4d2bef7894169eb
Cyprien BELLEMIN MENARD <belleminmenardcyprien@hotmail.fr>
2022-02-22 15:02:32 +0100

    Création et travail sur la branche développement / Mise en place du fichier app.js avec ajout des module sequelize,dotenv,bodyparser,helmet et connection à la BDD mySQL /  ébauche des premières routes dans app.js

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,42 @@
 const express = require('express');
+const { Sequelize } = require('sequelize');
+require('dotenv').config()
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+
+const User = process.env.DB_USER;
+const Password = process.env.DB_PASSWORD;
+
+const sequelize = new Sequelize("groupomania", `${User}`, `${Password}`, {
+  dialect: "mysql",
+  host: "localhost"
+});
+try {
+  sequelize.authenticate();
+  console.log('Connecté à la base de données MySQL!');
+} catch (error) {
+  console.error('Impossible de se connecter, erreur suivante :', error);
+}
 
 const app = express();
+
+app.use(helmet({
+  crossOriginResourcePolicy: false,
+}));
+
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(bodyParser.json());
+
+app.use('/image/gif', express.static('gif'));
+app.use('/image/images', express.static('images'));
+
+// app.use('/api/auth', authRoutes);
+// app.use('/api/post', postRoutes);
 
 module.exports = app;
