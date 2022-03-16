39f3825e1b248a27931452edc9d5548e9e9aa0ed
Cyprien BELLEMIN MENARD <belleminmenardcyprien@hotmail.fr>
2022-03-16 17:29:52 +0100

    Suppressio d'un commentaire sur la page loggin

--- frontend/src/redux/login/loginReducer.js
+++ frontend/src/redux/login/loginReducer.js
@@ -28,3 +28,4 @@ export const loginFunction = (user) => (dispatch) => {
       console.log(response)
     });
 };
+Ajout d'un système de cookie -> modification du back et du front en consequence pour generer le cookie lors du loggin et récuperer ces données lors des différentes requêtes // Début de création de la page register// Ajout d'un paragraphe sur la page loggin pour la réception et l'affichage d'erreur lors du contatc API // Passage sur axios pour le contact d'API
