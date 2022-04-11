261a389c3e00568c5dcc2442ec8489a0da824aae
Cyprien BELLEMIN MENARD <belleminmenardcyprien@hotmail.fr>
2022-04-11 16:31:36 +0200

    Mise en page : Début du responsive avec les média querie et window.innerWidth pour la navBar et la page home

--- frontend/src/Component/Navbar/Navbar.js
+++ frontend/src/Component/Navbar/Navbar.js
@@ -4,7 +4,8 @@ import { Link, useNavigate } from 'react-router-dom';
 import { getUserFunction } from '../../redux/user/userReducer';
 import { useDispatch, useSelector } from 'react-redux';
 import { logoutFunction } from '../../redux/user/userReducer';
-import logo from '../../Assets/logo/icon-left-font-monochrome-black.png';
+import logoFull from '../../Assets/logo/icon-left-font-monochrome-black.png';
+import logoOnly from '../../Assets/logo/icon-black.png';
 import houseLogo from '../../Assets/logo/house.png';
 import offline from '../../Assets/logo/offline-button.png';
 
@@ -15,16 +16,28 @@ export default function Navbar(props) {
 
   const logState = useSelector((state) => state.userReducer.status);
 
+  const [windowWidth, setWindowWidth] = useState(0)
+
+  const handleWindowWidth = () => {
+    setWindowWidth(window.innerWidth)
+  }
+
+  window.addEventListener('resize', handleWindowWidth)
+
+  console.log(windowWidth);
+
   useEffect(() => {
     if (logState === 'Session terminée') {
       navigate('/login');
     }
   }, [logState]);
 
+  console.log(window.innerWidth);
+
   return (
     <nav>
       <div className='nav__left-block'>
-        <img src={logo} alt='Logo groupomania' className='nav-logo' />
+        <img src={windowWidth > 767 ? logoFull : logoOnly} alt='Logo groupomania' className='nav-logo' />
         <Link to='/home'>
           <img
             src={houseLogo}
@@ -37,9 +50,10 @@ export default function Navbar(props) {
         <Link className='user-link' to={`/user/${props.userData.idUSER}`}>
           <div className='user-insert'>
             <img src={props.userData.pictureUrl} alt='Photo de profil' />
-            <p>
+            {windowWidth > 767 &&
+              <p>
               {props.userData.nickname}
-            </p>
+            </p>}
           </div>
         </Link>
         <button
